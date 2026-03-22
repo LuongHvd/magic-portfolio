70ac5af7141acad7d223fbeafb966a0a00035264
LuongHvd <91370135+LuongHvd@users.noreply.github.com>
2026-03-22 21:32:22 +0700

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -110,7 +110,7 @@ const home = {
   path: "/",
   image: "/images/og/home.png",
   label: "Home",
-  title: `${person.name}&apos;s Portfolio`,
+  title: `${person.name}’s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
   headline: <>Struggling means you are learning</>,
   featured: {
